0c115b08a89cd6f59a3385a47a99d3436d212fca
abkumar1 <AbKumar@prolifics.com>
2023-12-14 15:03:49 +0530

    bookrepo comments

--- src/main/java/com/multi/repo/BookRepo.java
+++ src/main/java/com/multi/repo/BookRepo.java
@@ -9,9 +9,19 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 
+/**
+ * Repository interface for managing Book entities.
+ */
 @Repository
 public interface BookRepo extends JpaRepository<Book,Long> {
 
+
+    /**
+     * Custom query to find books by the specified author name using a native SQL query.
+     *
+     * @param name The name of the author to search for.
+     * @return A list of Book entities that match the given author name.
+     */
     @Query(nativeQuery = true, value = "SELECT * FROM book b\n" +
             "JOIN book_authors ba ON b.id = ba.author_id\n" +
             "WHERE ba.author LIKE %:name%")
